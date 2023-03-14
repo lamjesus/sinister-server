bb73e57a86253b52d72c7e4dc113ecad7cbb9232
lamjesus <lam_jesus@hotmail.com>
2023-03-13 22:22:22 -0500

    feat: server 0.0.2 done

--- src/Server/Server.ts
+++ src/Server/Server.ts
@@ -1,22 +1,26 @@
 import http from 'http'
-import { CheckPort } from './CheckPort';
+
 
 export class Server {
     constructor(private port: number, private hostname: string) {
-
         this.port = port
         this.hostname = hostname
     }
 
-    async initServer() {
-        const portFree = await this.isPortFree()
-        if(portFree){
-
+     initServer() {
             const server = http.createServer((req, res) => {
-              
+                const server = http.createServer();
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'text/plain');
-                res.end('Hello, World!\n');
+                res.end('Hello, Fucking World!\n');
+                server.once('error', (err: any) => {
+                    if (err.code === 'EADDRINUSE') {
+                       throw new Error('puerto en uso')
+                    } else {
+                        throw new Error(err);
+                        
+                    }
+                });
             });
     
             server.listen(this.port, this.hostname, () => {
@@ -26,30 +30,28 @@ export class Server {
 
     }
 
-
     // check port 
-    private async isPortFree() {
-        try {
-            await new Promise((resolve, reject) => {
-                const server = http.createServer();
-                server.once('error', (err: any) => {
-                    if (err.code === 'EADDRINUSE') {
-                       throw new Error('puerto en uso')
-                    } else {
-                        reject(err);
-                    }
-                });
-                server.once('listening', () => {
-                    server.close();
-                    resolve(true);
-                });
-                server.listen(this.port);
-            });
-            return true;
-        } catch (error) {
+    // private async isPortFree() {
+    //     try {
+    //         await new Promise((resolve, reject) => {
+    //             const server = http.createServer();
+    //             server.once('error', (err: any) => {
+    //                 if (err.code === 'EADDRINUSE') {
+    //                    throw new Error('puerto en uso')
+    //                 } else {
+    //                     reject(err);
+    //                 }
+    //             });
+    //             server.once('listening', () => {
+    //                 server.close();
+    //                 resolve(true);
+    //             });
+    //             server.listen(this.port);
+    //         });
+    //         return true;
+    //     } catch (error) {
 
-        }
-    }
+    //     }
+    // }
 
 
-}
--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,7 @@ const hostname = '127.0.0.1';
 const port = 3240;
 
 const Sinister = new Server(port,hostname)
+
 Sinister.initServer()
+
 
